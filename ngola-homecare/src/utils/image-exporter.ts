d6e779cb3639330eb148ba92f944a46596437250
Joao Afonso Katombela <ja3328173@gmail.com>
2024-08-26 17:24:44 +0100

    feat: src/utils/image-exporter.ts. Criação de novo arquivo

---
+++ ngola-homecare/src/utils/image-exporter.ts
@@ -0,0 +1,289 @@
+// FIcheiro importador e exportador de imagens globalmente 
+//====================================================================================
+
+// imporando os backgrounds
+
+import bg_hero from "@/assets/bgs/Frame 427322806.png"
+import bg_login_instructor from "@/assets/bgs/Software-developers-vs-software-engineers-GettyImages-1533018011-e1708546433569.webp"
+import bg_hero_course from "@/assets/bgs/maxresdefault.jpg"
+import bg_free_course from "@/assets/bgs/free-online-courses-free-courses-in-england-1024x512.webp"
+import bg_certificate_course from "@/assets/bgs/edx-courses-free-online-courses-with-free-certificates.png"
+import bg_sec_section_no_bg from "@/assets/bgs/bg_teacheer_sec_section-removebg-preview.png"
+import bg_all_courses_hero from "@/assets/bgs/Frame 427322811.png"
+import bg_student_book from "@/assets/bgs/Frame 412.png"
+import bg_sec_section from "@/assets/bgs/bg teacheer sec section.png"
+import bg_course_cover from "@/assets/bgs/BG.png"
+import bg_free_courses_lines from "@/assets/bgs/Frame lines free courses.png"
+import bg_login from "@/assets/bgs/5790719 1.png"
+import bg_join_teaching from "@/assets/bgs/Image.png"
+import bg_corporate3_teaching from "@/assets/bgs/photo-1573496799515-eebbb63814f2.avif"
+import bg_corporate2_teaching from "@/assets/bgs/photo-1573497491765-dccce02b29df.avif"
+import bg_corporate1_teaching from "@/assets/bgs/photo-1560250097-0b93528c311a.avif"
+import bg_business from "@/assets/bgs/Frame 427322810.png"
+import bg_teacher from "@/assets/bgs/Frame 427322809.png"
+import bg_hero_shadow from "@/assets/bgs/Frame 427322811 shadow.png"
+import bg_all_program from "@/assets/bgs/Frame 427322811.png"
+import bg_teacher_page from "@/assets/bgs/Rectangle 2901 (1).png"
+import bg_teacher_page_no_bg from "@/assets/bgs/maxresdefault-removebg-preview.png"
+
+
+// imporando as imagens de bonecos 
+
+import doll_register from '@/assets/svg_persons/undraw_join_re_w1lh.svg'
+import doll_team_build from '@/assets/svg_persons/Team work-bro.svg'
+import doll_support_community from '@/assets/svg_persons/Collab-pana.svg'
+import doll_idea_lamp from '@/assets/svg_persons/Collab-bro.svg'
+import doll_teaching_coffee from '@/assets/svg_persons/Telecommuting-bro.svg'
+import doll_lesson_watch from '@/assets/svg_persons/Telecommuting-rafiki.svg'
+import doll_rcs_thumb from '@/assets/svg_persons/Good team-pana.svg'
+import doll_spirit_like from '@/assets/svg_persons/Team spirit-cuate.svg'
+import doll_team_spirit from '@/assets/svg_persons/Team spirit-rafiki.svg'
+import doll_team_community from '@/assets/svg_persons/Team spirit-pana.svg'
+import doll_typing from '@/assets/svg_persons/Typing-bro.svg'
+import doll_post_content from '@/assets/svg_persons/Blog post-bro.svg'
+import doll_register_2 from '@/assets/svg_persons/Filing system-rafiki.svg'
+import doll_login from '@/assets/svg_persons/Fingerprint-cuate (1).svg'
+
+
+// importando as categorias , as imagens  ou bgs 
+
+import bg_code from '@/assets/categories/Frame 405.png'
+import bg_line from '@/assets/categories/Frame 406.png'
+import bg_arduino from '@/assets/categories/Frame 407.png'
+import bg_3d from '@/assets/categories/Frame 408.png'
+
+
+// importando os logos da aplicacao icons e logo png e svg
+
+import logo_rcs from '@/assets/logos/Logo.png'
+import logo_success_icon from '@/assets/logos/images_logo_success.jpeg'
+import logo_paypal from '@/assets/logos/paypal-logo-payment-financial-transaction-ecommerce-payment-system-betaalwijze-text-line-rectangle-png-clipart.jpg'
+import logo_mcx from '@/assets/logos/D8372DBA-547A-40F7-A60E-3A491562D6D1.png'
+import logo_rcs_white from '@/assets/logos/Logo white.png'
+import logo_icon from '@/assets/logos/logo_rcs 2.png'
+
+
+// importar os svgs 
+import burger_svg from '@/assets/svgs/menu-2-svgrepo-com.svg'
+import material_loading_svg from '@/assets/svgs/mdi--loading.svg'
+import graduatedd_muslim_2_svg from '@/assets/svgs/Muslim graduation-cuate.svg'
+import online_lerarning_headset_svg from '@/assets/svgs/Online learning-pana.svg'
+import instructor_online_svg from '@/assets/svgs/Online learning-cuate.svg'
+import virtual_class_svg from '@/assets/svgs/Online learning-amico.svg'
+import walk_trough_books_svg from '@/assets/svgs/Learning-rafiki.svg'
+import spacial_study_svg from '@/assets/svgs/Learning-pana.svg'
+import muslim_graduate_svg from '@/assets/svgs/Muslim graduation-rafiki.svg'
+import person_popup_svg from '@/assets/svgs/Self checkout-bro.svg'
+import bg_sopnsor_svg from '@/assets/svgs/Career progress-pana.svg'
+import search_not_found_svg from '@/assets/svgs/Search-rafiki.svg'
+import bg_cart_svg from '@/assets/svgs/Credit card-cuate.svg'
+import image_preloader_svg from '@/assets/svgs/Images-rafiki.svg'
+import cart_svg from '@/assets/svgs/cart-large-2-svgrepo-com.svg'
+import not_found_svg from '@/assets/svgs/404 Error Page not Found with people connecting a plug-cuate.svg'
+import record_step_2_svg from '@/assets/svgs/record-svgrepo-com.svg'
+import video_step_3_svg from '@/assets/svgs/video-course-svgrepo-com.svg'
+import step_1_svg from '@/assets/svgs/study-process-svgrepo-com.svg'
+import burger_svg_filled from '@/assets/svgs/menu-navigation-grid-1529-svgrepo-com.svg'
+
+
+// imporando os backgrounds dos cards dos courses
+
+import bg_course_1 from "@/assets/bgs_courses/Image-1.png"
+import bg_course_2 from "@/assets/bgs_courses/Image-2.png"
+import bg_course_3 from "@/assets/bgs_courses/Image-3.png"
+import bg_course_4 from "@/assets/bgs_courses/Image.png"
+import bg_course_5 from "@/assets/bgs_courses/pexels-cats-coming-421999 1.png"
+import bg_course_6 from "@/assets/bgs_courses/pexels-lukas-590016 1-1.png"
+import bg_course_7 from "@/assets/bgs_courses/pexels-lukas-590016 1.png"
+import bg_course_8 from "@/assets/bgs_courses/pexels-olya-kobruseva-5561923 1-1.png"
+import bg_course_9 from "@/assets/bgs_courses/pexels-olya-kobruseva-5561923 1.png"
+import bg_course_10 from "@/assets/bgs_courses/pexels-pixabay-256417 1.png"
+import bg_course_11 from "@/assets/bgs_courses/pexels-shane-aldendorff-924676 1.png"
+import bg_course_12 from "@/assets/bgs_courses/unsplash_m_HRfLhgABo-1.png"
+import bg_course_13 from "@/assets/bgs_courses/unsplash_m_HRfLhgABo.png"
+
+
+// importar as imagens das companies parceiras 
+
+import hp_logo from "@/assets/companies/Rectangle 2874.png"
+import hik_logo from "@/assets/companies/Rectangle 2873.png"
+import cisco_logo from "@/assets/companies/Rectangle 2875.png"
+import autodesk_logo from "@/assets/companies/Rectangle 2876.png"
+import spectra_logo from "@/assets/companies/Rectangle 2877.png"
+import dell_logo from "@/assets/companies/Rectangle 2886.png"
+
+
+// imporando os cards com numeros peos no figma
+
+import card_1 from "@/assets/number-cards/Group 2554.png"
+import card_2 from "@/assets/number-cards/Group 2555.png"
+import card_3 from "@/assets/number-cards/Group 2556.png"
+import card_4 from "@/assets/number-cards/Group 2554-1.png"
+import card_5 from "@/assets/number-cards/Group 2555-1.png"
+import card_6 from "@/assets/number-cards/Group 2556-1.png"
+
+
+
+// importando os users pegos no FIgma 
+
+import user_female_1 from "@/assets/users/Avatar.png"
+import user_katumbela from "@/assets/users/1718265676368.jpeg"
+import user_female_2 from "@/assets/users/Avatar-3.png"
+import user_male_3 from "@/assets/users/Avatar-1.png"
+import user_male_4 from "@/assets/users/Avatar-2.png"
+
+
+
+// importando os icones 
+
+import briefcase_icon from '@/assets/icons/Briefcase.png'
+import ppl_speaking_icon from '@/assets/icons/Group 518.png'
+import devices_icon from '@/assets/icons/Icon.png'
+import device_web_icon from '@/assets/icons/Icon-1.png'
+import grafic_icon from '@/assets/icons/Icon-2.png'
+import idea_lamp_icon from '@/assets/icons/idea.png'
+import support_icon from '@/assets/icons/Vector-1.png'
+import star_blue_icon from '@/assets/icons/Star 4.png'
+import lang_icon from '@/assets/icons/Frame 4.png'
+
+
+
+
+// ============================================================================================== //
+// Exportação dos ficheiros 
+
+
+export const bgs = {
+  bg_hero,
+  bg_login,
+  bg_hero_shadow,
+  bg_certificate_course,
+  bg_sec_section,
+  bg_business,
+  bg_teacher_page_no_bg,
+  bg_corporate1_teaching,
+  bg_corporate2_teaching,
+  bg_corporate3_teaching,
+  bg_teacher_page,
+  bg_sec_section_no_bg,
+  bg_free_courses_lines,
+  bg_hero_course,
+  bg_free_course,
+  bg_login_instructor,
+  bg_all_program,
+  bg_teacher,
+  bg_join_teaching,
+  bg_course_cover,
+  bg_student_book,
+  bg_all_courses_hero
+}
+
+export const bonecos = {
+  doll_register,
+  doll_register_2,
+  doll_login,
+  doll_typing,
+  doll_team_community,
+  doll_post_content,
+  doll_spirit_like,
+  doll_idea_lamp,
+  doll_support_community,
+  doll_rcs_thumb,
+  doll_team_build,
+  doll_teaching_coffee,
+  doll_team_spirit,
+  doll_lesson_watch
+}
+
+export const logos = {
+  logo_rcs_white,
+  logo_rcs,
+  logo_success_icon,
+  logo_icon,
+  logo_paypal,
+  logo_mcx
+}
+
+export const svgs = {
+  burger_svg,
+  person_popup_svg,
+  image_preloader_svg,
+  instructor_online_svg,
+  walk_trough_books_svg,
+  spacial_study_svg,
+  muslim_graduate_svg,
+  graduatedd_muslim_2_svg,
+  virtual_class_svg,
+  online_lerarning_headset_svg,
+  step_1_svg,
+  cart_svg,
+  material_loading_svg,
+  search_not_found_svg,
+  bg_cart_svg,
+  bg_sopnsor_svg,
+  not_found_svg,
+  record_step_2_svg,
+  video_step_3_svg,
+  burger_svg_filled
+}
+
+export const companies = {
+  hp_logo,
+  dell_logo,
+  spectra_logo,
+  autodesk_logo,
+  cisco_logo,
+  hik_logo
+}
+
+export const bgs_courses = {
+  bg_course_1,
+  bg_course_2,
+  bg_course_3,
+  bg_course_4,
+  bg_course_6,
+  bg_course_5,
+  bg_course_7,
+  bg_course_8,
+  bg_course_9,
+  bg_course_10,
+  bg_course_11,
+  bg_course_12,
+  bg_course_13
+}
+
+export const cards_number = {
+  card_1,
+  card_2,
+  card_3,
+  card_4,
+  card_5,
+  card_6
+}
+
+export const users = {
+  user_female_1,
+  user_female_2,
+  user_katumbela,
+  user_male_3,
+  user_male_4
+}
+
+export const bg_categories = {
+  bg_3d,
+  bg_arduino,
+  bg_code,
+  bg_line
+}
+
+export const icons = {
+  briefcase_icon,
+  lang_icon,
+  support_icon,
+  ppl_speaking_icon,
+  device_web_icon,
+  devices_icon,
+  star_blue_icon,
+  idea_lamp_icon,
+  grafic_icon
+}
